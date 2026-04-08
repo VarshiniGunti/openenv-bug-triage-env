11d5609837a67cbcff60eb6c39e8bd466c1fa257
VarshiniGunti <guntiv@iitbhilai.ac.in>
2026-04-09 00:11:44 +0530

    build: add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Setup script for openenv-bug-triage-env."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="openenv-bug-triage-env",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires=">=3.11",
+)
